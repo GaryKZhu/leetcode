3539374b13500212677720b8d1501142c376ad36
GaryKZhu <39391898+GaryKZhu@users.noreply.github.com>
2018-11-20 19:19:36 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,16 @@
+#Solution 1
 class Solution:
     def twoSum(self, nums, target):
         for i in range(0,len(nums)):
             for j in range(0,len(nums)):
                 if((nums[i] + nums[j]) == target):
                     return i,j
+                
+#Solution 2
+class Solution:
+    def twoSum(self, nums, target):
+        dict = {}
+        for index, num in enumerate(nums):
+            if target - num in dict:
+                return [dict[target - num], index]
+            dict[num] = index 
