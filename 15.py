a6c12661ac0b431f50dc27bd701eea8a81823aa4
GaryKZhu <39391898+GaryKZhu@users.noreply.github.com>
2018-12-12 17:59:05 -0500

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,4 +1,10 @@
-#Brute Force
+#Brute Force(Time Limit Exceeded)
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
 solution = []
         answer = []
         for x in range(0, len(nums)):
@@ -17,3 +23,63 @@ solution = []
                             continue
         return solution
         
+#Two Pointer Solution
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        #sort list
+        nums.sort()
+        #run through exceptions
+        if len(nums) < 3:
+            return []
+        if all(v == 0 for v in nums):
+            return [[0,0,0]]
+        #initalize result list
+        result = []
+        
+        #loop through nums
+        
+        for i in range(len(nums)):
+            #if i is the same as previous value
+            
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+                
+                
+            #set target, which is negative i
+            
+            target = nums[i] * -1
+            
+            #set pointers
+            
+            leftpoint = i+1
+            rightpoint = len(nums)-1
+            
+            #if leftpoint is smaller than rightpoint
+            
+            while leftpoint<rightpoint:
+                #if they add up to negative i append
+                if nums[leftpoint]+nums[rightpoint] == target:
+                    result.append([nums[i], nums[leftpoint], nums[rightpoint]])
+                    
+                    #increase leftpoint value, as there still might be more values
+                    leftpoint += 1
+                    
+                    #if leftpoint is equal to one before it(or if multiple are)
+                    while leftpoint < rightpoint and nums[leftpoint] == nums[leftpoint-1]:
+                        leftpoint += 1
+                    
+                        
+                #if the added value is smaller, than increase leftpoint value
+                elif nums[leftpoint] + nums[rightpoint] < target:
+                    leftpoint = leftpoint+1
+                    
+                #if the added value is larger, than decrease rightpoint value
+                else:
+                    rightpoint = rightpoint-1
+                    
+        return result
